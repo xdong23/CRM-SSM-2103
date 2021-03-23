b4ceb4c9c48ce6929a943acd9434597314752831
xdong25 <15360085945@163.com>
2021-03-23 18:55:23 +0800

    123

--- src/main/java/com/dong/bean/Person.java
+++ src/main/java/com/dong/bean/Person.java
@@ -3,9 +3,10 @@ package com.dong.bean;
 /**
  * @author daDong
  * @version 1.0
- * @description
  * @date 2021-03-22-23:11
  */
 public class Person {
-
+    private String da;
+    private String zhong;
+    private String xiao;
 }
